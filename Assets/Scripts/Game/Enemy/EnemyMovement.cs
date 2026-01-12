e8b0edfcdb8e9e63e169718333a4bc4212401607
JackBillingham <billingham.jacks@gmail.com>
2026-01-12 23:31:27 +0100

    enemy behavior

--- Assets/Scripts/Game/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Game/Enemy/EnemyMovement.cs
@@ -9,11 +9,13 @@ public class EnemyMovement : MonoBehaviour
     private Rigidbody2D rb;
     private PlayerAwareness playerAwareness;
     private Vector2 targetDirection;
+    private float _changeDirectionCooldown;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         playerAwareness = GetComponent<PlayerAwareness>();
+        targetDirection = transform.up;
     }
 
     private void FixedUpdate()
@@ -22,26 +24,8 @@ public class EnemyMovement : MonoBehaviour
         RotateTowardsTarget();
         SetVelocity();
     }
-    
-    private void UpdateTargetDirection()
-    {
-        if (playerAwareness.AwareOfPlayer)
-        {
-            targetDirection = playerAwareness.DirectionToPlayer;
-        }
-        else
-        {
-            targetDirection = Vector2.zero;
-        }
-    }
-
     private void RotateTowardsTarget()
     {
-        if (targetDirection == Vector2.zero)
-        {
-            return;
-        }
-        
         Quaternion targetRotation = Quaternion.LookRotation(transform.forward, targetDirection);
         Quaternion rotation = Quaternion.RotateTowards(transform.rotation,  targetRotation, rotationSpeed * Time.deltaTime );
         
@@ -50,13 +34,41 @@ public class EnemyMovement : MonoBehaviour
 
     private void SetVelocity()
     {
-        if (targetDirection == Vector2.zero)
-        {
-            rb.linearVelocity = Vector2.zero;
-        }
-        else
         {
             rb.linearVelocity = transform.up * speed;
+        }
+    }
+
+    private void UpdateTargetDirection()
+    { 
+        if (playerAwareness.AwareOfPlayer)
+             {
+                 targetDirection = playerAwareness.DirectionToPlayer;
+             }
+             
+        HandleRandomDirectionChange();
+        HandlePlayerTargeting();
+    }
+
+    private void HandlePlayerTargeting()
+    {
+        if (playerAwareness.AwareOfPlayer)
+        {
+            targetDirection = playerAwareness.DirectionToPlayer;
+        }
+    }
+
+    private void HandleRandomDirectionChange()
+    {
+        _changeDirectionCooldown -= Time.deltaTime;
+
+        if (_changeDirectionCooldown <= 0)
+        {
+            float angleChange = Random.Range(-90f, 90f);
+            Quaternion rotaion = Quaternion.AngleAxis(angleChange, transform.forward);
+            targetDirection = rotaion * targetDirection;
+
+            _changeDirectionCooldown = Random.Range(1f, 5f);
         }
     }
 }
